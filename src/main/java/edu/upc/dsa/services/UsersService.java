e3471e2098b366e690885779647069ab87faf202
Didac <didac.arevalo@estudiantat.upc.edu>
2024-04-24 18:18:36 +0200

    SERVICIO API REST FUNCIONANDO - Falta programar enlace con html

--- src/main/java/edu/upc/dsa/services/UsersService.java
+++ src/main/java/edu/upc/dsa/services/UsersService.java
@@ -33,13 +33,53 @@ public class UsersService {
     @ApiResponses(value = {
             @ApiResponse(code = 201, message = "Successful", response = User.class, responseContainer = "List")
     })
-    @Path("/")
+    @Path("/getUsers")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getUsers() {
         List<User> users = this.ul.getUsers();
 
         GenericEntity<List<User>> entity = new GenericEntity<List<User>>(users) {};
         return Response.status(201).entity(entity).build();
+    }
+    @GET
+    @ApiOperation(value = "get User", notes = "---")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successful", response = User.class),
+            @ApiResponse(code = 404, message = "User not found")
+    })
+    @Path("/getUser/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUser(@PathParam("id") String id) {
+        User user = this.ul.getUser(id);
+        if(user==null) return Response.status(404).build();
+        else return Response.status(201).entity(user).build();
+    }
+
+    @POST
+    @ApiOperation(value = "create a new User", notes = "---")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successful", response=User.class),
+            @ApiResponse(code = 500, message = "Validation error")
+    })
+    @Path("/newUser")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response newUser(User user) {
+        User newUser = new User(user.getUserName(), user.getPassword());
+        if(newUser.getUserName()==null || newUser.getPassword()==null) return Response.status(500).entity(newUser).build();
+        this.ul.addUser(newUser);
+        return Response.status(201).entity(newUser).build();
+    }
+    @PUT
+    @ApiOperation(value = "update User Password", notes = "---")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successful"),
+            @ApiResponse(code = 404, message = "User not found")
+    })
+    @Path("/updateUserPassword")
+    public Response updateUserPassword(User user) {
+        User u = this.ul.updatePassword(user,user.getPassword());
+        if(u==null) return Response.status(404).build();
+        return Response.status(201).build();
     }
 
 }
