8cbef698beae3e6da12ddd7483b62f1249fed026
AliciaCarmonaLopez <146934397+AliciaCarmonaLopez@users.noreply.github.com>
2024-05-23 13:17:27 +0200

    Fichero base de datos

---
+++ bbdd.sql
@@ -0,0 +1,86 @@
+-- MariaDB dump 10.19-11.3.2-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: KebabSimulatorDB
+-- ------------------------------------------------------
+-- Server version	11.3.2-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `idUser` varchar(50) DEFAULT NULL,
+  `userName` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES
+('0','Alicia','1234','alicia@gmail.com'),
+('1','Marcos','1234','marcos@gmail.com'),
+('2222','Bigotes','1234','bigotes@email.com'),
+('eQwTIPzqH001798','Bruno','1234','bruno@gmail.com'),
+('l9x5273178237151015','Bruno','1234','bruno@gmail.com'),
+('FhDAMV216699162455273546','Bruno','1234','bruno@gmail.com'),
+('ogKu9848','Lucas','1234','lucas@gmail.com');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `weapon`
+--
+
+DROP TABLE IF EXISTS `weapon`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `weapon` (
+  `idWeapon` varchar(50) DEFAULT NULL,
+  `weaponName` varchar(50) DEFAULT NULL,
+  `description` varchar(50) DEFAULT NULL,
+  `damage` int(11) DEFAULT NULL,
+  `price` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `weapon`
+--
+
+LOCK TABLES `weapon` WRITE;
+/*!40000 ALTER TABLE `weapon` DISABLE KEYS */;
+INSERT INTO `weapon` VALUES
+('12hhd','Cuchillo','afilado',3,2.5);
+/*!40000 ALTER TABLE `weapon` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-23 13:05:44
