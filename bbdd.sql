82eaaa1daa2616feadd80cff5f86db61b5f35777
AliciaCarmonaLopez <146934397+AliciaCarmonaLopez@users.noreply.github.com>
2024-06-16 11:41:23 +0200

    Base de datos con todas las tablas

--- bbdd.sql
+++ bbdd.sql
@@ -16,38 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user` (
-  `idUser` varchar(50) DEFAULT NULL,
-  `userName` varchar(50) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL,
-  `email` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES
-('0','Alicia','1234','alicia@gmail.com'),
-('1','Marcos','1234','marcos@gmail.com'),
-('2222','Bigotes','1234','bigotes@email.com'),
-('eQwTIPzqH001798','Bruno','1234','bruno@gmail.com'),
-('l9x5273178237151015','Bruno','1234','bruno@gmail.com'),
-('FhDAMV216699162455273546','Bruno','1234','bruno@gmail.com'),
-('ogKu9848','Lucas','1234','lucas@gmail.com');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `ability`
 --
 
@@ -55,11 +23,13 @@ DROP TABLE IF EXISTS `ability`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ability` (
-  `idWeapon` varchar(50) DEFAULT NULL,
-  `weaponName` varchar(50) DEFAULT NULL,
+  `idAbility` varchar(50) NOT NULL,
+  `abilityName` varchar(50) DEFAULT NULL,
   `description` varchar(50) DEFAULT NULL,
-  `damage` int(11) DEFAULT NULL,
-  `price` double DEFAULT NULL
+  `value` int(11) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  PRIMARY KEY (`idAbility`),
+  UNIQUE KEY `idAbility` (`idAbility`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -73,6 +43,142 @@ INSERT INTO `ability` VALUES
 ('12hhd','Cuchillo','afilado',3,2.5);
 /*!40000 ALTER TABLE `ability` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `enemy`
+--
+
+DROP TABLE IF EXISTS `enemy`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `enemy` (
+  `idEnemy` varchar(50) NOT NULL,
+  `speed` int(11) DEFAULT NULL,
+  `description` varchar(50) DEFAULT NULL,
+  `meat` int(11) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idEnemy`),
+  UNIQUE KEY `idEnemy` (`idEnemy`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `enemy`
+--
+
+LOCK TABLES `enemy` WRITE;
+/*!40000 ALTER TABLE `enemy` DISABLE KEYS */;
+/*!40000 ALTER TABLE `enemy` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mission`
+--
+
+DROP TABLE IF EXISTS `mission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mission` (
+  `description` varchar(50) DEFAULT NULL,
+  `reward` int(11) DEFAULT NULL,
+  `idMission` varchar(50) NOT NULL,
+  PRIMARY KEY (`idMission`),
+  UNIQUE KEY `idMission` (`idMission`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mission`
+--
+
+LOCK TABLES `mission` WRITE;
+/*!40000 ALTER TABLE `mission` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mission` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `player`
+--
+
+DROP TABLE IF EXISTS `player`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `player` (
+  `idPlayer` varchar(50) NOT NULL,
+  `userName` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `money` double DEFAULT 0,
+  `currentMission` int(11) DEFAULT 0,
+  `currentLevel` int(11) DEFAULT 0,
+  PRIMARY KEY (`idPlayer`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `userName` (`userName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `player`
+--
+
+LOCK TABLES `player` WRITE;
+/*!40000 ALTER TABLE `player` DISABLE KEYS */;
+INSERT INTO `player` VALUES
+('M1038138','Bruno','7777','bruno@gmail.com',0,0,0);
+/*!40000 ALTER TABLE `player` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `playersability`
+--
+
+DROP TABLE IF EXISTS `playersability`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `playersability` (
+  `idAbility` varchar(50) DEFAULT NULL,
+  `idPlayer` varchar(50) DEFAULT NULL,
+  KEY `fkIdAbility` (`idAbility`),
+  KEY `fkIdPlayer` (`idPlayer`),
+  CONSTRAINT `fkIdAbility` FOREIGN KEY (`idAbility`) REFERENCES `ability` (`idAbility`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fkIdPlayer` FOREIGN KEY (`idPlayer`) REFERENCES `player` (`idPlayer`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `playersability`
+--
+
+LOCK TABLES `playersability` WRITE;
+/*!40000 ALTER TABLE `playersability` DISABLE KEYS */;
+INSERT INTO `playersability` VALUES
+('12hhd','M1038138');
+/*!40000 ALTER TABLE `playersability` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ranking`
+--
+
+DROP TABLE IF EXISTS `ranking`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ranking` (
+  `idGame` varchar(50) NOT NULL,
+  `idPlayer` varchar(50) NOT NULL,
+  `money` varchar(50) NOT NULL,
+  PRIMARY KEY (`idGame`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ranking`
+--
+
+LOCK TABLES `ranking` WRITE;
+/*!40000 ALTER TABLE `ranking` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ranking` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -83,4 +189,5 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-23 13:05:44
+-- Dump completed on 2024-06-16 11:39:14
+
